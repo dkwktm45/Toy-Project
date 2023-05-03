8968669219ca8390318d77379538055bcc82965f
jin young lee <dkwktm45@naver.com>
2023-05-03 21:38:37 +0900

    기록웹 프로젝트 시작git status

---
+++ weight-training/src/test/java/com/example/weighttraining/WeightTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weighttraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
